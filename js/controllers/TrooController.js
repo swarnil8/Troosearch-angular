c88ddc0f2ea64ada8a5e07c5b54506d48acc7e34
swarnil8 <swarnil8@gmail.com>
2017-07-09 14:30:26 +0530

    Add files via upload

---
+++ js/controllers/TrooController.js
@@ -0,0 +1,43 @@
+angular
+.module('TrooSearch')
+.controller('TrooController',['$scope',TrooFunction]);
+
+function TrooFunction( $scope ){
+
+  $scope.search = function( e ){
+    if(e.key == undefined)
+    $scope.check = false;
+    else {
+      $scope.check = true;
+    }
+
+  }
+
+
+  $scope.property = [{
+    name: 'Karmyogi Apartments',
+    location: 'Gurgaon',
+
+  },{
+    name: 'Shree Awas',
+    location: 'Dwarka',
+  },{
+    name: 'Classic Apartments',
+    location: 'Dwarka',
+  },{
+    name: 'Chitrakoot Dham ',
+    location: 'Dwarka',
+  },{
+    name: 'Sunny Valley',
+    location: 'Dwarka',
+  },{
+    name: 'Bhawalpur Apartments',
+    location: 'Dwarka',
+  }];
+
+  //$scope.change = [];
+  //$scope.click = function( e ){
+    //$scope.change.push($scope.searchText);
+  //}
+   
+}
